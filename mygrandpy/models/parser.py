350532b2a51a681ea6b13a3621cb446d398c2e41
Laurent GAUTRON <lg@Laurents-iMac.local>
2018-12-28 12:31:20 +0100

    regex to clean sentence and add words in stopword.json

--- mygrandpy/models/parser.py
+++ mygrandpy/models/parser.py
@@ -1,11 +1,14 @@
 import json
+import re
 
 
 class Parse:
 
     def __init__(self, sentence):
 
-        self.sentence = sentence.split(" ")
+        self.sentence = (re.sub(r"[,.;/:!_']", r" ", sentence))
+        self.sentence = (re.sub(r"[' ']{2,}", r" ", self.sentence))
+        self.sentence = self.sentence.split(" ")
 
     def select_word(self):
 
@@ -13,6 +16,8 @@ class Parse:
             stop_word_dict = json.load(stop_word)
         retained_text = []
         for word in self.sentence:
+            word = word.lower()
+            print(word)
             if word not in stop_word_dict:
                 retained_text.append(word)
         return retained_text
